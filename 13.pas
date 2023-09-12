825de9bfa97ab702c6a06ddc5e6da391fb16f2a0
я саша <144239395+aleksashaaleksasha@users.noreply.github.com>
2023-09-12 16:50:16 +0300

    Add files via upload

---
+++ 13.pas
@@ -0,0 +1,9 @@
+﻿Program Zad13;
+var a,b,c : integer;
+begin
+writeln('Введите 3 целых числа:');
+readln(a,b,c);
+if (a<=b) and (a<=c) then writeln('Наименьшее число из этих чисел - это ', a) else 
+  if (b<=a) and (b<=c) then writeln('Наименьшее число из этих чисел - это ', b) else 
+    if (c<=a) and (c<=b) then writeln('Наименьшее число из этих чисел - это ', c);
+end.
